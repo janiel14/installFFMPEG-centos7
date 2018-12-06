e57d722ff99c1797ecd9c29a85dcf879df2f9999
Janiel Oliveira <janielbelmont@gmail.com>
2018-12-06 11:00:19 -0200

    Create installFFMPEG.sh

---
+++ installFFMPEG.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+# dependency
+yum install autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel -y
+
+# Install folder
+mkdir ~/ffmpeg_sources -v
+
+# isntall freetype2
+FREETYPE_VER="2.9.1" #2018-05-02 08:42	2.4M	
+cd ~/ffmpeg_sources
+rm -vrf freetype-$FREETYPE_VER
+wget -N http://download.savannah.gnu.org/releases/freetype/freetype-$FREETYPE_VER.tar.gz
+tar -zxvf freetype-$FREETYPE_VER.tar.gz
+cd freetype-$FREETYPE_VER
+./configure --prefix="$HOME/ffmpeg_build" --enable-shared
+make -j
+make install
+ldconfig
+
+# install nasm
+cd ~/ffmpeg_sources
+curl -O -L http://www.nasm.us/pub/nasm/releasebuilds/2.13.02/nasm-2.13.02.tar.bz2
+tar xjvf nasm-2.13.02.tar.bz2
+cd nasm-2.13.02
+./autogen.sh
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+cp /root/bin/nasm /usr/bin/nasm -v
+cp /root/bin/nasm /usr/local/bin/nasm -v
+cp /root/bin/ndisasm /usr/bin/ndisasm -v
+cp /root/bin/ndisasm /usr/local/bin/ndisasm -v
+
+# install  yasm
+cd ~/ffmpeg_sources
+curl -O -L http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar xzvf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+cp /root/bin/yasm /usr/bin/yasm -v
+cp /root/bin/yasm /usr/local/bin/yasm -v
+cp /root/bin/ytasm /usr/bin/ytasm -v
+cp /root/bin/ytasm /usr/local/bin/ytasm -v
+
+# install h264
+cd ~/ffmpeg_sources
+git clone --depth 1 http://git.videolan.org/git/x264
+cd x264
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+make
+make install
+
+# install h265
+cd ~/ffmpeg_sources
+hg clone https://bitbucket.org/multicoreware/x265
+cd ~/ffmpeg_sources/x265/build/linux
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+make
+make install
+
+# install aac
+cd ~/ffmpeg_sources
+git clone --depth 1 https://github.com/mstorsjo/fdk-aac
+cd fdk-aac
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+# install mp3 #
+cd ~/ffmpeg_sources
+curl -O -L http://downloads.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz
+tar xzvf lame-3.100.tar.gz
+cd lame-3.100
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
+make
+make install
+
+#install opus
+cd ~/ffmpeg_sources
+curl -O -L https://archive.mozilla.org/pub/opus/opus-1.2.1.tar.gz
+tar xzvf opus-1.2.1.tar.gz
+cd opus-1.2.1
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+#install ogg
+cd ~/ffmpeg_sources
+curl -O -L http://downloads.xiph.org/releases/ogg/libogg-1.3.3.tar.gz
+tar xzvf libogg-1.3.3.tar.gz
+cd libogg-1.3.3
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+#install orbis
+cd ~/ffmpeg_sources
+curl -O -L http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
+tar xzvf libvorbis-1.3.5.tar.gz
+cd libvorbis-1.3.5
+./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+
+#install vpx
+cd ~/ffmpeg_sources
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
+cd libvpx
+./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm
+make
+make install
+
+#install ffmpeg with ssl
+cd ~/ffmpeg_sources && curl -O -L https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 && tar xjvf ffmpeg-snapshot.tar.bz2 && cd ffmpeg && PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+    --prefix="$HOME/ffmpeg_build" \
+    --pkg-config-flags="--static" \
+    --extra-cflags="-I$HOME/ffmpeg_build/include" \
+    --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+    --extra-libs=-lpthread \
+    --bindir="$HOME/bin" \
+    --enable-gpl \
+    --enable-libfdk_aac \
+    --enable-libfreetype \
+    --enable-libmp3lame \
+    --enable-libopus \
+    --enable-libvorbis \
+    --enable-libvpx \
+    --enable-libx264 \
+    --enable-libx265 \
+    --enable-openssl \
+    --enable-nonfree && make && make install && hash -r
+
+ffmpeg
+
+ffprobe
