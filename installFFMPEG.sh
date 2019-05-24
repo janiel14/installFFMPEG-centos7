e40ae187d2335dd0891ae2411d840db1e69d361a
Janiel Oliveira <janielbelmont@gmail.com>
2019-05-24 15:39:04 -0300

    Update installFFMPEG.sh

--- installFFMPEG.sh
+++ installFFMPEG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # dependency
-yum install autoconf automake bzip2 cmake hg freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel -y
+yum install autoconf automake bzip2 cmake hg freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel wget curl -y
 
 # Install folder
 mkdir ~/ffmpeg_sources -v
